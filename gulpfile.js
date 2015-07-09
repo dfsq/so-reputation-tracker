0cde82fd732a32c5de6e0966d6eecf71d2d5770b
dfsq <dfsq.dfsq@gmail.com>
2015-07-10 00:04:24 +0200

    some learning gulp babel routing

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
+
 var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-  // place code for your default task here
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat('app.es5.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('app/scripts/'));
 });
