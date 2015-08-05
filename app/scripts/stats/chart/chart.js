8b6d7c5245540cdf4323f1f3237341d468278af2
dfsq <dfsq.dfsq@gmail.com>
2015-08-05 08:51:28 +0200

    Line chart render API data mock

--- app/scripts/stats/chart/chart.js
+++ app/scripts/stats/chart/chart.js
@@ -21,23 +21,58 @@ export class Chart {
 
 	attached() {
 
-		nv.addGraph(() => {
-			var chart = nv.models.lineChart().useInteractiveGuideline(true);
+        const initialReputation = 0;
 
-			chart.xAxis
-				.axisLabel('Time (ms)')
-				.tickFormat(d3.format(',r'));
+        console.log(this.data);
 
-			chart.yAxis
-				.axisLabel('Voltage (v)')
-				.tickFormat(d3.format('.02f'));
+        nv.addGraph(() => {
+            var chart = nv.models.lineChart().options({
 
-			d3.select(this.chartContainer)
-				.datum(this.data)
-				.transition().duration(500)
-				.call(chart);
+                tooltips: true,
+                tooltipContent: function (key, x, y, e, graph) {
 
-			return chart;
-		});
+                    var delta = e.point[3] - planned,
+                        sign = delta > 0 ? '+' : '';
+
+                    return self.tooltipTemplate(key, {
+                        date: e.point[2],
+                        total: e.point[1] + initialReputation,
+                        planned: sign + delta,
+                        earned: e.point[3]
+                    });
+                },
+
+                isArea: true,
+                clipEdge: false,
+
+                x: function(d) {
+                    return d[0];
+                },
+                y: function(d) {
+                    return d[1] + initialReputation;
+                }
+            });
+
+            chart.xAxis
+                .axisLabel('Date')
+                .showMaxMin(true)
+                .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+
+            chart.yAxis
+                .axisLabel('Reputation')
+                .tickFormat(d3.format(','));
+
+            d3.select(this.chartContainer)
+                .datum([
+                    //{key: 'Planned', color: '#AEC7E8', values: [
+                    //    [Date.parse(options.startDate), 0, options.startDate],
+                    //    [Date.parse(options.endDate), options.goalReputation - initialReputation, options.endDate]
+                    //]},
+                    {key: 'Reputation', color: '#1F77B4', values: this.data},
+                ])
+                .transition().duration(500).call(chart);
+
+            nv.utils.windowResize(chart.update);
+        });
 	}
 }
--- app/scripts/stats/stats.js
+++ app/scripts/stats/stats.js
@@ -1,25 +1,14 @@
-function data() {
-	var sin = [],
-		cos = [];
-
-	for (var i = 0; i < 100; i++) {
-		sin.push({x: i, y: Math.sin(i/10)});
-		cos.push({x: i, y: .5 * Math.cos(i/10)});
-	}
-
-	return [
-		{
-			values: sin,
-			key: 'Sine Wave',
-			color: '#ff7f0e'
-		},
-		{
-			values: cos,
-			key: 'Cosine Wave',
-			color: '#2ca02c'
-		}
-	];
-}
+var data = [
+    [1436133600000,10,"06 Jul 2015",10],
+    [1436306400000,15,"08 Jul 2015",5],
+    [1436652000000,20,"12 Jul 2015",5],
+    [1436911200000,30,"15 Jul 2015",10],
+    [1437516000000,30,"22 Jul 2015",0],
+    [1437861600000,40,"26 Jul 2015",10],
+    [1438207200000,50,"30 Jul 2015",10],
+    [1438380000000,60,"01 Aug 2015",10],
+    [1438639200000,340,"04 Aug 2015",280]
+];
 
 export class Stats {
 
@@ -30,6 +19,6 @@ export class Stats {
 	}
 
 	activate() {
-		this.data = data();
+		this.data = data;
 	}
 }
