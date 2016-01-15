3bf9fee0143747dd999eeaf6e1ed7b93dad93056
Aliaksandr Astashenkau <aliaksandra@backbase.com>
2016-01-15 11:38:01 +0100

    Better route configuration

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,24 @@
-import {inject} from 'aurelia-framework';
-import {UserInfo} from './common/userInfo';
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Configuration } from './common/configuration';
 
-@inject(UserInfo)
+@inject(Router, Configuration)
 export class App {
 
-	constructor(userInfo) {
-		this.userInfo = userInfo;
+	// https://github.com/aurelia/router/issues/60#issuecomment-101421389
+	constructor(router, configuration) {
+		this.configuration = configuration.get();
 	}
 
 	configureRouter(config, router) {
 
-		const userInfo = this.userInfo;
+		const user = this.configuration.userId;
+
+		config.title = 'Stackoverflow Reputation Tracker';
 
 		config.addPipelineStep('authorize', {
 			run: function(navigationInstruction, next) {
 				if (navigationInstruction.getAllInstructions().some(i => i.config.checkConfig)) {
-					var user = userInfo.getUser();
 					if (!user) {
 						return next.cancel(router.navigate('config'));
 					}
@@ -35,14 +38,12 @@ export class App {
 				route: 'stats/:userId/:startDate',
 				name: 'stats',
 				moduleId: './stats/stats',
-				title: 'Reputation Stats',
+				title: 'Statistics',
 				checkConfig: true
 			},
 			{route: '', redirect: 'stats'}
 		]);
 
-		config.mapUnknownRoutes(instruction => {
-			router.navigate('config');
-		});
+		config.mapUnknownRoutes(instruction => './config/config');
 	}
 }
